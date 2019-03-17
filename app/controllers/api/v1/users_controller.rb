82f677819308f0fca6969d40e4acca1d5f0ad940
sojurner <pykim0591@gmail.com>
2019-03-16 18:50:42 -0600

    Add users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,22 +3,28 @@ class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:destroy, :show, :edit, :update]
 
   def index 
-    @users = User.all
+    users = User.all.map {|user|
+      {
+        user_name: user.user_name,
+        full_name: user.full_name,
+        email: user.email
+      }
+    }
+    render json: {users: users}
   end
 
   def new
     @user = User.new
   end
 
+  def show
+    render json: user_json
+  end
+
   def create
-    @user = User.new(
-      full_name: params["full_name"],
-      user_name: params["user_name"],
-      email: params["email"],
-      password: params["password"]
-    )
+    @user = User.new(full_name: params["full_name"], user_name: params["user_name"], email: params["email"], password: params["password"])
     if @user.save
-      render json: @user
+      render json: user_json
     else
       render json: {msg: 'Unable to create user entry'}, status: 400
     end
@@ -29,9 +35,6 @@ class Api::V1::UsersController < ApplicationController
     render json: {msg: "User has been removed"}
   end
   
-  def show 
-  end
-  
   def update
     if @user.update(user_params)
       render json: {msg: "Account has been Updated"}
@@ -39,11 +42,20 @@ class Api::V1::UsersController < ApplicationController
       render json: {msg: "Accout Could not be updated"}
     end
   end
+
   
   private
-  
+
   def set_user 
-    @user = User.find(params[:id])
+    @user = User.find(params[:id].to_i)
+  end
+
+  def user_json
+    {
+      user_name: @user.user_name,
+      full_name: @user.full_name,
+      email: @user.email
+    }
   end
 
   def user_params
