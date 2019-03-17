3902b3fc3cda26172516ff5eb633141ae87e1d26
sojurner <pykim0591@gmail.com>
2019-03-16 18:50:17 -0600

    Add player_stats_controller

---
+++ app/controllers/api/v1/player_stats_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::PlayerStatsController < ApplicationController
+  def index
+    if !params[:page]
+      starting_index = 25
+      players = PlayerStat.limit(starting_index)
+    else
+      range =  params[:page].to_i * 25
+      starting_index = range - 25
+      players = PlayerStat.limit(25).offset(starting_index)
+    end
+    render json: players
+  end
+end
