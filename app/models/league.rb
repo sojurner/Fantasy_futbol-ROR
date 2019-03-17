573865ff331ce3c58fc7098a4c9c8be67b41a2dc
sojurner <pykim0591@gmail.com>
2019-03-16 18:50:52 -0600

    Add League model

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,8 +1,6 @@
 class League < ApplicationRecord
   has_many :league_users
-  has_many :league_players
   has_many :users, through: :league_users
-  has_many :players, through: :league_players
 
-  validates_presence_of :name
+  validates :name, presence: true, uniqueness: {case_sensitive: false}
 end
