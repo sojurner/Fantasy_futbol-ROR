7399d8d88e6f48d215ef15e0acb4dbdc2025f553
sojurner <pykim0591@gmail.com>
2019-03-16 18:50:35 -0600

    Add sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create, :update, :destroy]
+
   def new
   end
   
@@ -6,7 +8,13 @@ class Api::V1::SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)  
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      render json: {msg: "Welcome #{user.full_name}", user: user}
+      render json: {
+        msg: "Welcome #{user.full_name}", 
+        full_name: user.full_name, 
+        user_name: user.user_name, 
+        email: user.email,
+        session_id: user.id
+      }
     else
       render json: {msg: "Invalid Email and/or Password"}
     end
