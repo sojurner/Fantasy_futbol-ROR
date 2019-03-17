67a991637742242429fe3bc935be232449969959
sojurner <pykim0591@gmail.com>
2019-03-16 18:51:33 -0600

    Add player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,6 @@
 class Player < ApplicationRecord
-  validates_presence_of :user_id
-  validates_presence_of :league_id
-  validates_presence_of :player_id
+  has_many :league_user_players
+  has_many :league_users, through: :league_user_players
+  belongs_to :player_stat
+  validates :player_stat_id, presence: true, uniqueness: true
 end
